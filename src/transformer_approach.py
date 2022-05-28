b18fbc0be1198904d6c2c2e72166a98018ee82ee
Silleellie <antonio.silletti@gmail.com>
2022-05-28 11:48:20 +0200

    implemented compute_predictions() method

--- src/transformer_approach.py
+++ src/transformer_approach.py
@@ -2,13 +2,8 @@ import datasets
 import numpy as np
 import pandas as pd
 import torch.cuda
-from ray import tune
-from ray.tune.schedulers import PopulationBasedTraining
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
-from transformers import DataCollatorWithPadding
-from transformers import TrainingArguments
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from flair.data import Sentence
 from flair.models import SequenceTagger
 
@@ -86,132 +81,46 @@ class TransformersApproach:
 
         return {'Pos': tags}
 
+    def compute_prediction(self, output_file='./submission_1.csv'):
+        def tokenize_fn_padding(tokenizer, batch_item_dataset):
+            return tokenizer(batch_item_dataset["Phrase"], batch_item_dataset["Pos"], truncation=True, padding=True)
 
-def run_hyperparameters(model_name, train_file_path, cpu_number, gpu_number):
-    def compute_metrics(eval_preds):
-        logits, labels = eval_preds
-        predictions = np.argmax(logits, axis=-1)
+        def compute_prediction(single_item):
+            ids = torch.tensor(single_item['input_ids'], dtype=torch.int32).to(device).unsqueeze(dim=0)
+            token_type_ids = torch.tensor(single_item['token_type_ids'], dtype=torch.int32).to(device).unsqueeze(dim=0)
+            mask = torch.tensor(single_item['attention_mask'], dtype=torch.int32).to(device).unsqueeze(dim=0)
 
-        return {'sklearn_accuracy': accuracy_score(labels, predictions)}
+            logits = self.model(input_ids=ids, token_type_ids=token_type_ids, attention_mask=mask)
+            prediction = np.argmax(torch.flatten(logits.logits).to('cpu').detach().numpy(), axis=-1)
 
-    t = TransformersApproach(model_name)
+            return {'pred': prediction}
 
-    dataset = t.dataset_builder(train_file_path, cut=15000)
+        test_set_path = '../dataset/test.tsv'
 
-    dataset_pos = dataset.map(lambda single_item_dataset: t.pos_tagger_fn(single_item_dataset))
+        test = pd.read_csv(test_set_path, sep="\t")
 
-    dataset_tokenized = dataset_pos.map(lambda single_item_dataset: t.tokenize_fn(single_item_dataset),
-                                        batched=True)
+        test_dict = {'Phrase': list(test["Phrase"])}
 
-    # this specific model expects label column
-    dataset_formatted = dataset_tokenized.rename_column('Sentiment', 'label').remove_columns(['Phrase', "Pos"])
+        dataset_dict = datasets.Dataset.from_dict(test_dict)
 
-    data_collator = DataCollatorWithPadding(tokenizer=t.tokenizer)
+        dataset_pos = dataset_dict.map(lambda single_item_dataset: self.pos_tagger_fn(single_item_dataset))
 
-    training_args = TrainingArguments("../output/test-trainer",
-                                      evaluation_strategy='epoch',
-                                      num_train_epochs=5,
-                                      optim='adamw_torch',
-                                      per_device_train_batch_size=16,
-                                      per_device_eval_batch_size=16,
-                                      disable_tqdm=True,
-                                      save_total_limit=3,
-                                      save_strategy='epoch')
+        dataset_tokenized = dataset_pos.map(lambda single_item_dataset: tokenize_fn_padding(self.tokenizer,
+                                                                                            single_item_dataset),
+                                            batched=True)
 
-    trainer = Trainer(
-        model_init=lambda: t.model_init(),
-        args=training_args,
-        train_dataset=dataset_formatted["train"],
-        eval_dataset=dataset_formatted["validation"],
-        data_collator=data_collator,
-        tokenizer=t.tokenizer,
-        compute_metrics=compute_metrics
-    )
+        # this specific model expects label column
+        dataset_formatted = dataset_tokenized.remove_columns(['Phrase', "Pos"])
 
-    tune_config = {
-        "per_device_train_batch_size": tune.choice([4, 8, 16, 32, 64]),
-        "num_train_epochs": tune.choice([2, 3, 4, 5]),
-        "seed": tune.randint(0, 43),
-        "weight_decay": tune.uniform(0.0, 0.3),
-        "learning_rate": tune.uniform(1e-4, 5e-5),
-        "lr_scheduler_type": tune.choice(['linear', 'cosine', 'polynomial'])
-    }
+        result = dataset_formatted.map(compute_prediction)
 
-    pbt_scheduler = PopulationBasedTraining(
-        time_attr="training_iteration",
-        metric="eval_sklearn_accuracy",
-        mode="max",
-        perturbation_interval=1,
-        hyperparam_mutations={
-            "per_device_train_batch_size": tune.choice([4, 8, 16, 32, 64]),
-            "num_train_epochs": [2, 3, 4, 5],
-            "seed": tune.randint(0, 43),
-            "weight_decay": tune.uniform(0.0, 0.3),
-            "learning_rate": tune.uniform(1e-4, 5e-5),
-            "lr_scheduler_type": ['linear', 'cosine', 'polynomial']  # list and no 'choice' otherwise continuous error
-        })
+        final_dict = {'PhraseId': test['PhraseId'], 'Sentiment': result['pred']}
+        final_df = pd.DataFrame(final_dict)
 
-    best_trial = trainer.hyperparameter_search(
-        hp_space=lambda _: tune_config,
-        direction="maximize",
-        backend="ray",
-        scheduler=pbt_scheduler,
-        n_trials=6,
-        resources_per_trial={"cpu": cpu_number, "gpu": gpu_number},
-        keep_checkpoints_num=1,
-        local_dir="../hyper_search/",
-        name="tune_transformer_pbt"
-    )
-
-    return best_trial
-
-
-def final_train(model_name, train_file_path, best_trial):
-    def compute_metrics(eval_preds):
-        logits, labels = eval_preds
-        predictions = np.argmax(logits, axis=-1)
-
-        return {'sklearn_accuracy': accuracy_score(labels, predictions)}
-
-    t = TransformersApproach(model_name)
-
-    dataset = t.dataset_builder(train_file_path)
-
-    dataset_pos = dataset.map(lambda single_item_dataset: t.pos_tagger_fn(single_item_dataset))
-
-    dataset_tokenized = dataset_pos.map(lambda single_item_dataset: t.tokenize_fn(single_item_dataset),
-                                        batched=True)
-
-    # this specific model expects label column
-    dataset_formatted = dataset_tokenized.rename_column('Sentiment', 'label').remove_columns(['Phrase', "Pos"])
-
-    data_collator = DataCollatorWithPadding(tokenizer=t.tokenizer)
-
-    training_args = TrainingArguments("../output/test-trainer",
-                                      evaluation_strategy='epoch',
-                                      num_train_epochs=5,
-                                      optim='adamw_torch',
-                                      per_device_train_batch_size=16,
-                                      per_device_eval_batch_size=16,
-                                      disable_tqdm=True,
-                                      save_total_limit=3,
-                                      save_strategy='epoch')
-
-    trainer = Trainer(
-        model_init=lambda: t.model_init(),
-        args=training_args,
-        train_dataset=dataset_formatted["train"],
-        eval_dataset=dataset_formatted["validation"],
-        data_collator=data_collator,
-        tokenizer=t.tokenizer,
-        compute_metrics=compute_metrics
-    )
-
-    for n, v in best_trial.hyperparameters.items():
-        setattr(trainer.args, n, v)
-
-    return trainer.train()
+        final_df.to_csv(output_file, index=False)
 
 
 if __name__ == '__main__':
-    run_hyperparameters('bert-base-uncased', '../dataset/train.tsv', 4, 1)
+
+    t = TransformersApproach('checkpoint-15606')
+    t.compute_prediction('submission_1.csv')
