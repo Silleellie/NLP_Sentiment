eae3f1a2614211b8527e78ab11dd09285691fab3
Silleellie <antonio.silletti@gmail.com>
2022-05-28 11:48:47 +0200

    better organization for hyperparameters tuning (to improve)

--- src/hyperparameters_tuning.py
+++ src/hyperparameters_tuning.py
@@ -1,95 +1,22 @@
-import os
-from typing import Optional, List
-
 import numpy as np
 from ray import tune
-
-import transformers
-from sklearn.metrics import accuracy_score
-from transformers import TrainingArguments, DataCollatorWithPadding
-from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
-from ray.tune import CLIReporter
 from ray.tune.schedulers import PopulationBasedTraining
-
-import torch
+from sklearn.metrics import accuracy_score
+from transformers import Trainer, TrainingArguments, DataCollatorWithPadding
 
 from src.transformer_approach import TransformersApproach
 
 
-class TuneTransformerTrainer(transformers.Trainer):
-
-    def evaluate(self, eval_dataset=None, ignore_keys: Optional[List[str]] = None, metric_key_prefix: str = "eval"):
-
-        eval_dataloader = self.get_eval_dataloader(eval_dataset)
-        output = self.evaluation_loop(eval_dataloader,
-                                      description="Evaluation",
-                                      ignore_keys=ignore_keys,
-                                      metric_key_prefix=metric_key_prefix)
-        self.log(output.metrics)
-
-        self.save_state()
-
-        tune.report(**output.metrics)
-
-        return output.metrics
-
-    def save_state(self):
-        with tune.checkpoint_dir(step=self.state.global_step) as checkpoint_dir:
-            self.args.output_dir = checkpoint_dir
-            # This is the directory name that Huggingface requires.
-            output_dir = os.path.join(
-                self.args.output_dir,
-                f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}")
-            self.save_model(output_dir)
-            if self.is_world_process_zero():
-                torch.save(self.optimizer.state_dict(),
-                           os.path.join(output_dir, "optimizer.pt"))
-                torch.save(self.lr_scheduler.state_dict(),
-                           os.path.join(output_dir, "scheduler.pt"))
-
-
-def recover_checkpoint(tune_checkpoint_dir, model_name=None):
-    if tune_checkpoint_dir is None or len(tune_checkpoint_dir) == 0:
-        return model_name
-    # Get subdirectory used for Huggingface.
-    subdirs = [
-        os.path.join(tune_checkpoint_dir, name)
-        for name in os.listdir(tune_checkpoint_dir)
-        if os.path.isdir(os.path.join(tune_checkpoint_dir, name))
-    ]
-    # There should only be 1 subdir.
-    assert len(subdirs) == 1, subdirs
-    return subdirs[0]
-
-
-def train_transformer_hyperparam(config, checkpoint_dir=None):
+def run_hyperparameters(model_name, train_file_path, cpu_number, gpu_number):
     def compute_metrics(eval_preds):
         logits, labels = eval_preds
         predictions = np.argmax(logits, axis=-1)
 
         return {'sklearn_accuracy': accuracy_score(labels, predictions)}
 
-    training_args = TrainingArguments(
-        output_dir=tune.get_trial_dir(),
-        learning_rate=config["learning_rate"],
-        do_train=True,
-        do_eval=True,
-        evaluation_strategy="epoch",
-        # We explicitly set save to 0, and do checkpointing in evaluate instead
-        save_steps=0,
-        num_train_epochs=config["num_epochs"],
-        per_device_train_batch_size=config["per_gpu_train_batch_size"],
-        per_device_eval_batch_size=config["per_gpu_val_batch_size"],
-        warmup_steps=0,
-        weight_decay=config["weight_decay"],
-        logging_dir="./logs",
-    )
+    t = TransformersApproach(model_name)
 
-    model_name_or_path = recover_checkpoint(checkpoint_dir, config["model_name"])
-
-    t = TransformersApproach(model_name_or_path)
-
-    dataset = config['dataset']
+    dataset = t.dataset_builder(train_file_path, cut=15000)
 
     dataset_pos = dataset.map(lambda single_item_dataset: t.pos_tagger_fn(single_item_dataset))
 
@@ -97,78 +24,105 @@ def train_transformer_hyperparam(config, checkpoint_dir=None):
                                         batched=True)
 
     # this specific model expects label column
-    dataset_formatted = dataset_tokenized.rename_column('Sentiment', 'label').remove_columns(['Phrase', 'Pos'])
+    dataset_formatted = dataset_tokenized.rename_column('Sentiment', 'label').remove_columns(['Phrase', "Pos"])
 
     data_collator = DataCollatorWithPadding(tokenizer=t.tokenizer)
 
-    # Use our modified TuneTransformerTrainer
-    tune_trainer = TuneTransformerTrainer(
-        model=t.model,
+    training_args = TrainingArguments("../output/test-trainer",
+                                      evaluation_strategy='epoch',
+                                      num_train_epochs=5,
+                                      optim='adamw_torch',
+                                      per_device_train_batch_size=16,
+                                      per_device_eval_batch_size=16,
+                                      disable_tqdm=True,
+                                      save_total_limit=3,
+                                      save_strategy='epoch')
+
+    trainer = Trainer(
+        model_init=lambda: t.model_init(),
         args=training_args,
-        train_dataset=dataset_formatted['train'],
-        eval_dataset=dataset_formatted['validation'],
-        compute_metrics=compute_metrics,
+        train_dataset=dataset_formatted["train"],
+        eval_dataset=dataset_formatted["validation"],
         data_collator=data_collator,
+        tokenizer=t.tokenizer,
+        compute_metrics=compute_metrics
     )
-    tune_trainer.train()
 
-
-def hyper_run(model_name):
-
-    dataset = TransformersApproach.dataset_builder('../dataset/train.tsv', cut=100)
-
-    config = {
-        # These 3 configs below were defined earlier
-        "model_name": model_name,
-        "data_dir": "test",
-        "per_gpu_val_batch_size": 32,
-        "per_gpu_train_batch_size": tune.choice([4, 8, 16, 32, 64]),
-        "learning_rate": tune.uniform(1e-5, 5e-5),
+    tune_config = {
+        "per_device_train_batch_size": tune.choice([4, 8, 16, 32, 64]),
+        "num_train_epochs": tune.choice([2, 3, 4, 5]),
+        "seed": tune.randint(0, 43),
         "weight_decay": tune.uniform(0.0, 0.3),
-        "num_epochs": tune.choice([2, 3, 4, 5]),
-        "dataset": dataset
+        "learning_rate": tune.uniform(1e-4, 5e-5),
+        "lr_scheduler_type": tune.choice(['linear', 'cosine', 'polynomial'])
     }
 
-    scheduler = PopulationBasedTraining(
+    pbt_scheduler = PopulationBasedTraining(
         time_attr="training_iteration",
         metric="eval_sklearn_accuracy",
         mode="max",
-        perturbation_interval=2,
+        perturbation_interval=1,
         hyperparam_mutations={
-            "weight_decay": lambda: tune.uniform(0.0, 0.3).func(None),
-            "learning_rate": lambda: tune.uniform(1e-5, 5e-5).func(None),
-            "per_gpu_train_batch_size": [16, 32, 64],
+            "per_device_train_batch_size": tune.choice([4, 8, 16, 32, 64]),
+            "num_train_epochs": [2, 3, 4, 5],
+            "seed": tune.randint(0, 43),
+            "weight_decay": tune.uniform(0.0, 0.3),
+            "learning_rate": tune.uniform(1e-4, 5e-5),
+            "lr_scheduler_type": ['linear', 'cosine', 'polynomial']  # list and no 'choice' otherwise continuous error
         })
 
-    reporter = CLIReporter(
-        parameter_columns={
-            "weight_decay": "w_decay",
-            "learning_rate": "lr",
-            "per_gpu_train_batch_size": "train_bs/gpu",
-            "num_epochs": "num_epochs"
-        },
-        metric_columns=[
-            "eval_sklearn_accuracy", "eval_loss", "epoch", "training_iteration"
-        ])
+    best_trial = trainer.hyperparameter_search(
+        hp_space=lambda _: tune_config,
+        direction="maximize",
+        backend="ray",
+        scheduler=pbt_scheduler,
+        n_trials=6,
+        resources_per_trial={"cpu": cpu_number, "gpu": gpu_number},
+        keep_checkpoints_num=1,
+        local_dir="../hyper_search/",
+        name="tune_transformer_pbt"
+    )
 
-    analysis = tune.run(
-        train_transformer_hyperparam,
-        resources_per_trial={
-            "cpu": 2,
-            "gpu": 1
-        },
-        config=config,
-        num_samples=10,
-        scheduler=scheduler,
-        keep_checkpoints_num=3,
-        checkpoint_score_attr="training_iteration",
-        progress_reporter=reporter,
-        local_dir="./ray_results/",
-        name="tune_transformer_pbt")
-
-    best_config = analysis.get_best_config(metric="eval_sklearn_accuracy", mode="max")
-    print(best_config)
+    return best_trial
 
 
-if __name__ == '__main__':
-    hyper_run('albert-base-v2')
+def my_final_train(model_name, train_file_path, best_trial):
+    def compute_metrics(eval_preds):
+        logits, labels = eval_preds
+        predictions = np.argmax(logits, axis=-1)
+        return {'sklearn_accuracy': accuracy_score(labels, predictions)}
+
+    t = TransformersApproach(model_name)
+    dataset = t.dataset_builder(train_file_path)
+    dataset_pos = dataset.map(lambda single_item_dataset: t.pos_tagger_fn(single_item_dataset))
+    dataset_tokenized = dataset_pos.map(lambda single_item_dataset: t.tokenize_fn(single_item_dataset),
+                                        batched=True)
+
+    # this specific model expects label column
+    dataset_formatted = dataset_tokenized.rename_column('Sentiment', 'label').remove_columns(['Phrase', "Pos"])
+    data_collator = DataCollatorWithPadding(tokenizer=t.tokenizer)
+    training_args = TrainingArguments("../output/test-trainer",
+                                      evaluation_strategy='epoch',
+                                      num_train_epochs=5,
+                                      optim='adamw_torch',
+                                      per_device_train_batch_size=16,
+                                      per_device_eval_batch_size=16,
+                                      save_total_limit=3,
+                                      save_strategy='epoch')
+
+    trainer = Trainer(
+        model_init=lambda: t.model_init(),
+        args=training_args,
+        train_dataset=dataset_formatted["train"],
+        eval_dataset=dataset_formatted["validation"],
+        data_collator=data_collator,
+        tokenizer=t.tokenizer,
+        compute_metrics=compute_metrics
+    )
+
+    for n, v in best_trial.hyperparameters.items():
+        setattr(trainer.args, n, v)
+
+    trainer.train()
+
+    return trainer
