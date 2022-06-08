c45e13ce4d42c919b0c3b4a58f902c8772297c90
Elio Musacchio <elio.musacchio28@gmail.com>
2022-06-08 17:52:22 +0200

    defined main for sbert approaches

--- src/sbert/sentence_transformers_approaches.py
+++ src/sbert/sentence_transformers_approaches.py
@@ -13,20 +13,6 @@ Clustering approach: 0.5264321414840446
 MLP approach: 0.595924644367551
 """
 
-model = SentenceTransformer('all-mpnet-base-v2')
-
-train = pd.read_csv('../../dataset/train.tsv', sep="\t")
-texts = train['Phrase'].to_list()[:10000]
-labels = train['Sentiment'].to_list()[:10000]
-
-train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels,
-                                                                                train_size=.8,
-                                                                                stratify=labels,
-                                                                                random_state=42)
-
-train_embeddings = model.encode(train_texts, convert_to_tensor=True)
-test_embeddings = model.encode(test_texts, convert_to_tensor=True)
-
 def clusters_approach(train_embeddings, train_labels, test_embeddings):
     clusters = {}
     for label in [0, 1, 2, 3, 4]:
@@ -51,5 +37,27 @@ def MLP_approach(train_embeddings, train_labels, test_embeddings):
     clf = MLPClassifier(random_state=42, max_iter=500).fit(train_embeddings.cpu(), train_labels)
     return clf.predict(test_embeddings.cpu())
 
-print("CLUSTERING APPROACH: ", accuracy_score(test_labels, clusters_approach(train_embeddings, train_labels, test_embeddings)))
-print("MLP APPROACH: ", accuracy_score(test_labels, MLP_approach(train_embeddings, train_labels, test_embeddings)))
+
+if __name__ == "__main__":
+
+    """
+    These approaches were not considered for the real test set since the accuracy
+    obtained on the experiment was too low
+    """
+        
+    model = SentenceTransformer('all-mpnet-base-v2')
+
+    train = pd.read_csv('../../dataset/train.tsv', sep="\t")
+    texts = train['Phrase'].to_list()[:10000]
+    labels = train['Sentiment'].to_list()[:10000]
+
+    train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels,
+                                                                            train_size=.8,
+                                                                            stratify=labels,
+                                                                            random_state=42)
+
+    train_embeddings = model.encode(train_texts, convert_to_tensor=True)
+    test_embeddings = model.encode(test_texts, convert_to_tensor=True)
+
+    print("CLUSTERING APPROACH: ", accuracy_score(test_labels, clusters_approach(train_embeddings, train_labels, test_embeddings)))
+    print("MLP APPROACH: ", accuracy_score(test_labels, MLP_approach(train_embeddings, train_labels, test_embeddings)))
