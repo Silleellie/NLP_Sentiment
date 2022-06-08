1b521f5d601dff5afe41679dcb174523723af331
Elio Musacchio <elio.musacchio28@gmail.com>
2022-06-08 17:31:30 +0200

    restructured svc approach

--- src/svc/svc_approach.py
+++ src/svc/svc_approach.py
@@ -1,9 +1,6 @@
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-
 import spacy
 
 model = 'en_core_web_sm'
@@ -20,36 +17,33 @@ def preprocessing(phrase: str):
     token_list = []
     for w in phrase_data:
         if not w.ent_type_:
-            if not w.is_punct:
-                token_list.append(w.lemma_.lower())
+            token_list.append(w.lemma_.lower())
         else:
             token_list.append('<' + w.ent_type_ + '>')
     return token_list
 
 
-train = pd.read_csv("train.tsv", sep="\t")
-# test = pd.read_csv("test.tsv", sep="\t")
-X = train['Phrase'].tolist()
-Y = train['Sentiment'].tolist()
+if __name__ == '__main__':
+    train = pd.read_csv("../../dataset/train.tsv", sep="\t")
+    test = pd.read_csv("../../dataset/test.tsv", sep="\t")
+    X_train = train['Phrase'].tolist()
+    Y_train = train['Sentiment'].tolist()
+    X_test = test['Phrase'].tolist()
+    phrase_id = test['PhraseId'].tolist()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.2)
-phrase_id = train['PhraseId'].tolist()[-len(Y_test):]
+    vectorizer = CountVectorizer(binary=True, analyzer=preprocessing)
+    X_train = vectorizer.fit_transform(X_train)
 
-vectorizer = CountVectorizer(binary=True, analyzer=preprocessing)
-X_train = vectorizer.fit_transform([phrase for phrase in X_train])
+    classif = SVC()
+    classif.fit(X_train, Y_train)
 
-classif = SVC()
-classif.fit(X_train, Y_train)
+    Y_pred = []
 
-Y_pred = []
+    for phrase, x in zip(X_test, classif.predict(vectorizer.transform(X_test))):
+        print("SCORE: ", x)
+        print("PHRASE: ", phrase)
+        Y_pred.append(x)
 
-for phrase, x in zip(X_test, classif.predict(vectorizer.transform([phrase for phrase in X_test]))):
-    print("SCORE: ", x)
-    print("PHRASE: ", phrase)
-    Y_pred.append(x)
-
-print(accuracy_score(Y_test, Y_pred))
-
-columns = {'PhraseId': phrase_id, 'Sentiment': Y_pred}
-df = pd.DataFrame(data=columns)
-df.to_csv("output.csv", index=False)
+    columns = {'PhraseId': phrase_id, 'Sentiment': Y_pred}
+    df = pd.DataFrame(data=columns)
+    df.to_csv("submission.csv", index=False)
