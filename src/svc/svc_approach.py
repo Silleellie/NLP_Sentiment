40d44efcc945d965f93c6211458a2943a7573456
Elio Musacchio <elio.musacchio28@gmail.com>
2022-06-08 19:50:21 +0200

    created class for svc approach

--- src/svc/svc_approach.py
+++ src/svc/svc_approach.py
@@ -3,24 +3,41 @@ from sklearn.svm import SVC
 from sklearn.feature_extraction.text import CountVectorizer
 import spacy
 
-model = 'en_core_web_sm'
+class SVCApproach:
 
-if model not in spacy.cli.info()['pipelines']:
-    spacy.cli.download(model)
+    def __init__(self, model = 'en_core_web_sm', **args):
+        if model not in spacy.cli.info()['pipelines']:
+            spacy.cli.download(model)
 
-nlp = spacy.load(model)
-nlp.add_pipe('merge_entities')
+        self.nlp = spacy.load(model)
+        self.nlp.add_pipe('merge_entities')
 
+        self.vectorizer = CountVectorizer(binary=True, analyzer=self.preprocessing)
 
-def preprocessing(phrase: str):
-    phrase_data = list(nlp(phrase))
-    token_list = []
-    for w in phrase_data:
-        if not w.ent_type_:
-            token_list.append(w.lemma_.lower())
-        else:
-            token_list.append('<' + w.ent_type_ + '>')
-    return token_list
+        self.svc = SVC(**args)
+
+    def preprocessing(self, phrase: str):
+        phrase_data = list(self.nlp(phrase))
+        token_list = []
+        for w in phrase_data:
+            if not w.ent_type_:
+                token_list.append(w.lemma_.lower())
+            else:
+                token_list.append('<' + w.ent_type_ + '>')
+        return token_list
+    
+    def train(self, train_texts, train_labels):
+        train_vectors = self.vectorizer.fit_transform(train_texts)
+        self.svc.fit(train_vectors, train_labels)
+    
+    def predict(self, test_texts):
+        return self.svc.predict(self.vectorizer.transform(test_texts))
+    
+    def test_and_submit(self, test_texts, test_ids):
+        predictions = self.predict(test_texts)
+        columns = {'PhraseId': test_ids, 'Sentiment': predictions}
+        df = pd.DataFrame(data=columns)
+        df.to_csv("submission.csv", index=False)
 
 
 if __name__ == '__main__':
@@ -29,21 +46,8 @@ if __name__ == '__main__':
     X_train = train['Phrase'].tolist()
     Y_train = train['Sentiment'].tolist()
     X_test = test['Phrase'].tolist()
-    phrase_id = test['PhraseId'].tolist()
+    phrase_ids = test['PhraseId'].tolist()
 
-    vectorizer = CountVectorizer(binary=True, analyzer=preprocessing)
-    X_train = vectorizer.fit_transform(X_train)
-
-    classif = SVC()
-    classif.fit(X_train, Y_train)
-
-    Y_pred = []
-
-    for phrase, x in zip(X_test, classif.predict(vectorizer.transform(X_test))):
-        print("SCORE: ", x)
-        print("PHRASE: ", phrase)
-        Y_pred.append(x)
-
-    columns = {'PhraseId': phrase_id, 'Sentiment': Y_pred}
-    df = pd.DataFrame(data=columns)
-    df.to_csv("submission.csv", index=False)
+    svc = SVCApproach()
+    svc.train(X_train, Y_train)
+    svc.test_and_submit(X_test, phrase_ids)
